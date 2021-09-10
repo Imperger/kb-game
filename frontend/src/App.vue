8a027bad75529d640e681f015d26c2d477540e09
Imperger <mr.imperger@hotmail.com>
2021-09-10 12:59:18 +0300

    feat: Tune overall view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,11 +6,13 @@
 
 <style>
 #app {
+  display: flex;
   font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  min-height: 100vh;
 }
 
 #nav {
