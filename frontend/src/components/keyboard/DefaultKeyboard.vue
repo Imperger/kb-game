cde69ef9754a36c5aef6846421ae89bc7774833c
Imperger <mr.imperger@hotmail.com>
2021-09-20 15:11:03 +0300

    Close #7

--- frontend/src/components/keyboard/DefaultKeyboard.vue
+++ frontend/src/components/keyboard/DefaultKeyboard.vue
@@ -1,7 +1,7 @@
 <template>
 <svg xmlns="http://www.w3.org/2000/svg"
-    width="1400"
-    height="400"
+    width="1070"
+    height="240"
     role="presentation">
 
   <template v-for="(b, i) in numsLine">
