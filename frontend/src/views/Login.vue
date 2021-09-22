544cc4ac1bff0bc98a29362e755ee5c41af80f0a
Imperger <mr.imperger@hotmail.com>
2021-09-22 16:07:24 +0300

    feat: Reactive error message & lang selector

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,15 +1,25 @@
 <template>
-<form class="loginComponent">
-  <MyTextInput v-model="usernameOrEmail" :label="$t('auth.usernameOrEmail')" name="userid" v-validate="'username_or_email'" data-vv-delay="600"/>
-  <MyTextInput v-model="password" password :label="$t('auth.password')" name="password" v-validate="'required|length:8,100'" data-vv-delay="600"/>
-  <div class="loginButtonWrapper">
-    <MyButton @click="onSubmit" :disabled="loginButtonDisabled">{{ $t('auth.login') }}</MyButton>
-    <span class="loginError">{{ loginError }}</span>
-  </div>
-</form>
+<div class="loginComponent">
+  <AppLangSelector />
+  <form>
+    <MyTextInput v-model="usernameOrEmail" :label="$t('auth.usernameOrEmail')" name="userid" v-validate="'username_or_email'" data-vv-delay="600"/>
+    <MyTextInput v-model="password" password :label="$t('auth.password')" name="password" v-validate="'required|length:8,100'" data-vv-delay="600"/>
+    <div class="loginButtonWrapper">
+      <MyButton @click="onSubmit" :disabled="loginButtonDisabled">{{ $t('auth.login') }}</MyButton>
+      <span v-show="hasLoginErrorMessage" class="loginError">{{ loginError }}</span>
+    </div>
+  </form>
+</div>
 </template>
 
 <style scoped>
+
+.langSelector {
+  position: absolute;
+  right: 25px;
+  top: 25px;
+}
+
 .loginComponent {
   margin: auto;
 }
@@ -36,9 +46,11 @@ import MyButton from '@/components/MyButton.vue';
 import { isAxiosError } from '@/typeguards/axios-typeguard';
 import { LoginResponse } from '@/services/api-service/interfaces/login-response';
 import { StatusCode } from '@/services/api-service/types/status-code';
+import AppLangSelector from '@/components/AppLangSelector.vue';
 
 @Component({
   components: {
+    AppLangSelector,
     MyTextInput,
     MyButton
   }
@@ -48,10 +60,10 @@ export default class Login extends Mixins(ApiServiceMixin) {
 
   private password = '';
 
-  private loginError = '';
+  private loginErrorStatusCode: StatusCode | null = null;
 
   async onSubmit (): Promise<void> {
-    this.loginError = '';
+    this.loginErrorStatusCode = null;
 
     try {
       await this.$recaptchaLoaded();
@@ -59,18 +71,26 @@ export default class Login extends Mixins(ApiServiceMixin) {
       await this.api.login(this.usernameOrEmail, this.password, token);
     } catch (e) {
       if (isAxiosError<LoginResponse>(e)) {
-        const code = e.response?.data.code;
-        if (code === StatusCode.InvalidCredentials) {
-          this.loginError = this.$t('auth.invalidCredentials') as string;
-        } else if (code === StatusCode.PendingConfirmRegistration) {
-          this.loginError = this.$t('auth.pendingConfirmation') as string;
-        } else if (code === StatusCode.PendingConfirmRegistrationExpired) {
-          this.loginError = this.$t('auth.confirmationExpired') as string;
-        } else {
-          this.loginError = this.$t('auth.unknownError') as string;
-        }
+        this.loginErrorStatusCode = e.response?.data.code as StatusCode;
       }
     }
+  }
+
+  private get loginError () {
+    switch (this.loginErrorStatusCode) {
+      case StatusCode.InvalidCredentials:
+        return this.$t('auth.invalidCredentials') as string;
+      case StatusCode.PendingConfirmRegistration:
+        return this.$t('auth.pendingConfirmation') as string;
+      case StatusCode.PendingConfirmRegistrationExpired:
+        return this.$t('auth.confirmationExpired') as string;
+      default:
+        return this.$t('auth.unknownError') as string;
+    }
+  }
+
+  private get hasLoginErrorMessage () {
+    return this.loginErrorStatusCode !== null;
   }
 
   private get loginButtonDisabled () {
