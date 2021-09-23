505ffb9d469062c9ef74483f1b483039e6d95127
Imperger <mr.imperger@hotmail.com>
2021-09-23 17:55:12 +0300

    feat: Add updatedAt field to secret

--- backend/src/common/schemas/user.schema.ts
+++ backend/src/common/schemas/user.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema()
+@Schema({ timestamps: { createdAt: false } })
 class UserSecret {
     @Prop()
     salt: string;
