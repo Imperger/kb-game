88a81c008cf2f557eb4bddfac58a493ba271870f
Imperger <mr.imperger@hotmail.com>
2022-03-15 22:24:00 +0200

    refact: Describe scopes

--- backend/src/auth/scopes.ts
+++ backend/src/auth/scopes.ts
@@ -1,9 +1,27 @@
 export const scopeMetaId = Symbol('scope');
 
+/**
+ * User scopes
+ */
 export enum Scope { 
+    /**
+     * Possibility to edit scopes of another users
+     */
     AssignScope, 
+    /**
+     * Possibility to take part in games
+     */
     PlayGame, 
+    /**
+     * Possibility to write to non-game and game chat
+     */
     WriteToChat,
+    /**
+     * Possibility to manage game texts
+     */
     EditScenario, 
+    /**
+     * Possibility to mute users for some time period, remove messages, etc
+     */
     ModerateChat
 };
