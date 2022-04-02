32520d24fc92f82dcc026201a368439b7cb5fb92
Imperger <mr.imperger@hotmail.com>
2022-04-02 18:48:04 +0300

    feat: Switch to fastify + https

--- spawner/src/main.ts
+++ spawner/src/main.ts
@@ -1,8 +1,22 @@
+import * as fs from 'fs';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const httpsOptions = {
+    key: fs.readFileSync('./certs/server.key'),
+    cert: fs.readFileSync('./certs/server.cer'),
+  };
+
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ https: httpsOptions })); // TODO: Respect to config.ssl
+  
+  app.enableCors();
+
+  await app.listen(3001);
 }
 bootstrap();
