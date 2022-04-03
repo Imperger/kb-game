22e03141ef3e66989765b62eab335c1f410fd478
Imperger <mr.imperger@hotmail.com>
2022-04-03 17:53:10 +0300

    feat: Add name & capacity properties to config

--- spawner/src/config/config-schema.ts
+++ spawner/src/config/config-schema.ts
@@ -1,6 +1,14 @@
-import { IsString, IsDefined } from 'class-validator';
+import { IsString, IsDefined, IsNumber } from 'class-validator';
 
 export class ConfigSchema {
+  @IsDefined({ message: "missing 'name' property" })
+  @IsString()
+  readonly name: string;
+
+  @IsDefined({ message: "missing 'capacity' property" })
+  @IsNumber()
+  readonly capacity: number;
+
   @IsDefined({ message: "missing 'secret' property" })
   @IsString()
   readonly secret: string;
