7a3c88b052166e4657debdc59f568a19c3e52fde
Imperger <mr.imperger@hotmail.com>
2022-05-19 16:28:54 +0300

    refact: Missing semicolon

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,5 +1,5 @@
 import { ConfigLoader } from './config-loader';
-import launchParams from '../launch-params'
+import launchParams from '../launch-params';
 import { ConfigPopulateOptionals } from './config-populate-optionals';
 
 export default ConfigPopulateOptionals(new ConfigLoader().load(launchParams.config));
