89c054383cae88cc9bc8e2fe21ed8ac31101c5a3
Imperger <mr.imperger@hotmail.com>
2022-05-20 20:25:13 +0300

    refact: Correct describe

--- frontend/src/services/api-service/auth/types.ts
+++ frontend/src/services/api-service/auth/types.ts
@@ -1,14 +1,13 @@
 export enum StatusCode {
     Ok = 0,
     InternalError = 1,
-    // Authentication
     // Registration
     CaptchaFailed = 10,
     UsernameIsTaken = 11,
     EmailIsTaken = 12,
     MissingUserForConfirmRegistration = 13,
     RegistrationAlreadyConfirmed = 14,
-    // Login
+    // Authentication
     InvalidCredentials = 20,
     PendingConfirmRegistration = 21,
     PendingConfirmRegistrationExpired = 22
