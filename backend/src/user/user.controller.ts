2222d35760b3413d45010244efa8bea51d8478be
Imperger <mr.imperger@hotmail.com>
2022-05-20 23:31:19 +0300

    feat: Add user controller

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+
+import { JwtGuard } from '../jwt/decorators/jwt.guard';
+import { User } from '../auth/decorators/user';
+import { CurrentUserPipe } from './pipes/current-user.pipe';
+import { CurrentUser } from './interfaces/current-user';
+
+@Controller('user')
+export class UserController {
+    @UseGuards(JwtGuard)
+    @Get('me')
+    me(@User(CurrentUserPipe) user: CurrentUser) {
+      return user;
+  }
+}
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -4,12 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from './user.service';
 import { User, UserSchema } from '../common/schemas/user.schema';
 import { CurrentUserPipe } from './pipes/current-user.pipe';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
   ],
   providers: [UserService, CurrentUserPipe],
+  controllers: [UserController],
   exports: [UserService]
 })
 export class UserModule { }
