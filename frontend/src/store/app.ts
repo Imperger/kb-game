f7c679c89a92e690b7760f2f75437b7cd6e1ffca
Imperger <mr.imperger@hotmail.com>
2022-05-20 23:53:31 +0300

    refact: Rename access token var

--- frontend/src/store/app.ts
+++ frontend/src/store/app.ts
@@ -1,11 +1,20 @@
 import { Action, Module, Mutation, VuexModule } from 'vuex-module-decorators';
+import { Subject } from 'rxjs';
+import { first } from 'rxjs/operators';
 
-import { AvailableLocales } from '@/locales/available-locales';
-import { switchLocale } from '@/i18n';
+import { CurrentUser } from '@/services/api-service/user/types';
 
 @Module({ name: 'app' })
 export default class App extends VuexModule {
+    /**
+     * The application is considered initialized
+     * after an automatic authorization attempt, regardless of its result.
+     */
+    private initialized = false;
+    private $initialized = new Subject<void>();
+
     public authToken = '';
+    private me: CurrentUser | null = null;
 
     @Mutation
     public setToken (token: string): void {
@@ -17,7 +26,46 @@ export default class App extends VuexModule {
       this.authToken = '';
     }
 
+    @Mutation
+    public setUser (user: CurrentUser | null): void {
+      this.me = user;
+    }
+
+    @Mutation
+    public Initialize (): void {
+      this.initialized = true;
+      this.$initialized.next();
+    }
+
+    @Mutation
+    public Uninitialize (): void {
+      this.initialized = false;
+    }
+
+    @Action
+    public async waitInitializationFor (timeout: number): Promise<void> {
+      return this.initialized
+        ? Promise.resolve()
+        : new Promise((resolve, reject) => {
+          let timeoutTimer = -1;
+          if (timeout >= 0) {
+            timeoutTimer = setTimeout(() => reject(new Error('Timeut')), timeout);
+          }
+
+          this.$initialized
+            .pipe(first())
+            .subscribe(x => ((clearTimeout(timeoutTimer), resolve())));
+        });
+    }
+
     public get hasToken (): boolean {
       return this.authToken.length > 0;
+    }
+    public get user (): CurrentUser | null {
+      return this.me;
+    }
+
+    public get loggedIn (): boolean {
+      return this.me !== null;
     }
 }
