7f0081779a5f0d0c3209d77958211480485405ea
Imperger <mr.imperger@hotmail.com>
2022-05-22 19:58:19 +0300

    fix: Hack for outdated typescript

--- frontend/src/router/router-comparator.ts
+++ frontend/src/router/router-comparator.ts
@@ -1,20 +1,29 @@
 import { Location, Route } from 'vue-router/types/router';
 
+const isString = (x: unknown): x is string => {
+  return typeof x === 'string';
+};
+
+const isLocation = (x: unknown): x is Location => {
+  const t = x as Record<string, unknown>;
+  return ['name', 'path'].some(x => typeof t[x] === 'string');
+};
+
 export const isRoutesEqual = <T extends string | Location>(a: T, b: T): boolean => {
   if (typeof a !== typeof b) {
     return false;
   }
 
-  if (typeof a === 'string' || typeof b === 'string') {
+  if (isString(a)) {
     return a === b;
-  }
+  } else if (isLocation(a) && isLocation(b)) {
+    if (typeof a.name === 'string' && typeof b.name === 'string') {
+      return a.name === b.name;
+    }
 
-  if (typeof a.name === 'string' && typeof b.name === 'string') {
-    return a.name === b.name;
-  }
-
-  if (typeof a.path === 'string' && typeof b.path === 'string') {
-    return a.path === b.path;
+    if (typeof a.path === 'string' && typeof b.path === 'string') {
+      return a.path === b.path;
+    }
   }
 
   return false;
