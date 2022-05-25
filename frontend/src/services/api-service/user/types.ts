08fd1167c6ecad802c8816176b11e9c705a32795
Imperger <mr.imperger@hotmail.com>
2022-05-25 15:59:10 +0300

    feat: Update client user schema corresponds to backend

--- frontend/src/services/api-service/user/types.ts
+++ frontend/src/services/api-service/user/types.ts
@@ -5,6 +5,7 @@ export interface CurrentUser {
   avatar: string,
   scopes: {
     assignScope: boolean;
+    serverMaintainer: boolean;
     blockedUntil: Date;
     editScenario: boolean;
     moderateChat: boolean;
