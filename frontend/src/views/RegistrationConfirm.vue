b2546d4723c78f4ada241dc25e14d8331d40c2fb
Imperger <mr.imperger@hotmail.com>
2022-05-27 19:50:18 +0300

    feat: Use RejectedResponse instead catching exceptions

--- frontend/src/views/RegistrationConfirm.vue
+++ frontend/src/views/RegistrationConfirm.vue
@@ -11,6 +11,7 @@ import { Component, Mixins, Prop } from 'vue-property-decorator';
 import ApiServiceMixin from '@/mixins/api-service-mixin';
 import { isAxiosError } from '@/typeguards/axios-typeguard';
 import { RegistrationConfirmResponse } from '@/services/api-service/auth/types';
+import { isRejectedResponse } from '@/services/api-service/rejected-response';
 
 @Component
 export default class RegistrationConfirm extends Mixins(ApiServiceMixin) {
@@ -24,12 +25,12 @@ export default class RegistrationConfirm extends Mixins(ApiServiceMixin) {
       const response = await this.api.auth.confirmRegistration(this.code);
       if (response.code === 0) {
         this.result = 'Registration successfully completed';
-      } else {
-        this.result = response.message;
+      } else if (isRejectedResponse(response)) {
+        this.result = response?.message ?? 'Unknown error';
       }
     } catch (e: unknown) {
       if (isAxiosError<RegistrationConfirmResponse>(e)) {
-        this.result = e.response?.data.message || 'Unknown error';
+        this.result = e.response?.data.message ?? 'Unknown error';
       }
     }
   }
