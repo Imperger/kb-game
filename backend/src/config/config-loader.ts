253c90d21278c467c598fa71c9b93ec5a0153ff2
Imperger <mr.imperger@hotmail.com>
2022-05-28 11:36:43 +0300

    style: Linter pass

--- backend/src/config/config-loader.ts
+++ backend/src/config/config-loader.ts
@@ -5,20 +5,20 @@ import { ConfigSchema } from './config-schema';
 import { LogException } from '../common/logger-exception';
 
 export class ConfigLoader {
-    private readonly ex = new LogException('ConfigLoader');
-    private readonly validator = new ConfigValidator();
-    load(filename: string): ConfigSchema {
-        try {
-            const config = JSON.parse(fs.readFileSync(filename, 'utf8'));
-            this.validator.validate(config);
-            return config;
-        } catch (e) {
-            if (e.code === 'ENOENT')
-                this.ex.throw(`Missing config file '${filename}'`);
-            else if (e.name === 'SyntaxError')
-                this.ex.throw(`Config '${filename}' has malformed. Expected json.`);
-            else
-                throw e;
-        }
+  private readonly ex = new LogException('ConfigLoader');
+  private readonly validator = new ConfigValidator();
+  load(filename: string): ConfigSchema {
+    try {
+      const config = JSON.parse(fs.readFileSync(filename, 'utf8'));
+      this.validator.validate(config);
+      return config;
+    } catch (e) {
+      if (e.code === 'ENOENT')
+        this.ex.throw(`Missing config file '${filename}'`);
+      else if (e.name === 'SyntaxError')
+        this.ex.throw(`Config '${filename}' has malformed. Expected json.`);
+      else
+        throw e;
     }
+  }
 }
--- backend/src/config/config-schema.ts
+++ backend/src/config/config-schema.ts
@@ -2,73 +2,73 @@ import { IsString, ValidateNested, IsDefined, IsBoolean, IsNumber, IsOptional } 
 import { Type } from 'class-transformer';
 
 class Api {
-    @IsDefined({ message: 'missing property \'api.jwtSecret\'' })
-    @IsString()
-    readonly jwtSecret: string;
+  @IsDefined({ message: 'missing property \'api.jwtSecret\'' })
+  @IsString()
+  readonly jwtSecret: string;
 }
 class Mongo {
-    @IsDefined({ message: 'missing property \'mongo.connectionURI\'' })
-    @IsString()
-    readonly connectionURI: string;
+  @IsDefined({ message: 'missing property \'mongo.connectionURI\'' })
+  @IsString()
+  readonly connectionURI: string;
 }
 class Google {
-    @IsDefined({ message: 'missing property \'auth.google.clientId\'' })
-    @IsString()
-    readonly clientId: string;
+  @IsDefined({ message: 'missing property \'auth.google.clientId\'' })
+  @IsString()
+  readonly clientId: string;
 
-    @IsDefined({ message: 'missing property \'auth.google.secret\'' })
-    @IsString()
-    readonly secret: string;
+  @IsDefined({ message: 'missing property \'auth.google.secret\'' })
+  @IsString()
+  readonly secret: string;
 }
 class Auth {
-    @IsOptional()
-    @IsString()
-    readonly jwtSecret: string;
+  @IsOptional()
+  @IsString()
+  readonly jwtSecret: string;
 
-    @IsDefined({ message: 'missing \'auth.confirmCodeTtl\' property' })
-    @IsString()
-    readonly confirmCodeTtl: string;
+  @IsDefined({ message: 'missing \'auth.confirmCodeTtl\' property' })
+  @IsString()
+  readonly confirmCodeTtl: string;
 
-    @IsDefined({ message: 'missing \'auth.google\' scope' })
-    @ValidateNested()
-    @Type(() => Google)
-    readonly google: Google;
+  @IsDefined({ message: 'missing \'auth.google\' scope' })
+  @ValidateNested()
+  @Type(() => Google)
+  readonly google: Google;
 }
 class ReCaptcha {
-    @IsDefined({ message: 'missing property \'reCaptcha.secret\'' })
-    @IsString()
-    readonly secret: string;
+  @IsDefined({ message: 'missing property \'reCaptcha.secret\'' })
+  @IsString()
+  readonly secret: string;
 }
 export class ConfigSchema {
-    @IsDefined({ message: 'missing \'domain\' property' })
-    @IsString()
-    readonly domain: string;
+  @IsDefined({ message: 'missing \'domain\' property' })
+  @IsString()
+  readonly domain: string;
 
-    @IsDefined({ message: 'missing \'port\' property' })
-    @IsNumber()
-    readonly port: number;
+  @IsDefined({ message: 'missing \'port\' property' })
+  @IsNumber()
+  readonly port: number;
 
-    @IsDefined({ message: 'missing \'ssl\' property' })
-    @IsBoolean()
-    readonly ssl: boolean;
+  @IsDefined({ message: 'missing \'ssl\' property' })
+  @IsBoolean()
+  readonly ssl: boolean;
 
-    @IsDefined({ message: 'missing \'api\' scope' })
-    @ValidateNested()
-    @Type(() => Api)
-    readonly api: Api;
+  @IsDefined({ message: 'missing \'api\' scope' })
+  @ValidateNested()
+  @Type(() => Api)
+  readonly api: Api;
 
-    @IsDefined({ message: 'missing \'mongo\' scope' })
-    @ValidateNested()
-    @Type(() => Mongo)
-    readonly mongo: Mongo;
+  @IsDefined({ message: 'missing \'mongo\' scope' })
+  @ValidateNested()
+  @Type(() => Mongo)
+  readonly mongo: Mongo;
 
-    @IsDefined({ message: 'missing \'auth\' scope' })
-    @ValidateNested()
-    @Type(() => Auth)
-    readonly auth: Auth;
+  @IsDefined({ message: 'missing \'auth\' scope' })
+  @ValidateNested()
+  @Type(() => Auth)
+  readonly auth: Auth;
 
-    @IsDefined({ message: 'missing \'reCaptcha\' scope' })
-    @ValidateNested()
-    @Type(() => ReCaptcha)
-    readonly reCaptcha: ReCaptcha;
+  @IsDefined({ message: 'missing \'reCaptcha\' scope' })
+  @ValidateNested()
+  @Type(() => ReCaptcha)
+  readonly reCaptcha: ReCaptcha;
 }
