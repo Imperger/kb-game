e3cf451db7d150a8e150e8f274d975711434ce90
Imperger <mr.imperger@hotmail.com>
2022-05-29 23:57:20 +0300

    feat: Design play menu view

--- frontend/src/views/main-menu/MainMenuPlay.vue
+++ frontend/src/views/main-menu/MainMenuPlay.vue
@@ -1,16 +1,33 @@
 <template>
-<div>
-    Play
-</div>
+<v-container>
+    <v-row>
+        <v-col md="auto" class="pa-1">
+            <v-btn class="btn-tile" plain>Quik game</v-btn>
+        </v-col>
+        <v-col md="auto" class="pa-1">
+            <v-btn @click="newCustomGame" class="btn-tile" plain>Custom game</v-btn>
+        </v-col>
+    </v-row>
+</v-container>
 </template>
 
 <style scoped>
-
+.btn-tile {
+    width: 250px;
+    min-height: 140px;
+    color: white;
+    background-color: #039be5;
+}
 </style>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { ApiServiceMixin } from '@/mixins';
+import { Component, Mixins, Vue } from 'vue-property-decorator';
 
 @Component
-export default class MainMenuPlay extends Vue {}
+export default class MainMenuPlay extends Mixins(ApiServiceMixin) {
+  async newCustomGame (): Promise<void> {
+    this.api.game.newCustom();
+  }
+}
 </script>
