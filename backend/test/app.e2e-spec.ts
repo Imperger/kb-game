da5b5f3d4615d6970d3598d1c18af83f1c296218
Imperger <mr.imperger@hotmail.com>
2022-07-07 19:55:26 +0300

    test: Trying a e2e

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,24 +1,47 @@
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request = require('supertest');
+import { Model } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
+
 import { AppModule } from './../src/app.module';
+import { HttpExceptionFilter } from '@/common/filters/http-exception.filter';
+import { AppExceptionFilter } from '@/common/filters/app-exception/app-exception.filter';
+import { DtoValidationPipe } from '@/common/pipes/dto-validation.pipe';
+import { StatusCode } from '@/common/types/status-code';
+import { User } from '@/user/schemas/user.schema';
+import { userStub } from '@/user/test/stubs/user.stub';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let userModel: Model<User>;
+  const user = userStub();
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    userModel = moduleFixture.get<Model<User>>(getModelToken(User.name));
+
     app = moduleFixture.createNestApplication();
+    app.useLogger(false);
+    app.enableCors();
+    app.setGlobalPrefix('api');
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalFilters(new AppExceptionFilter());
+    app.useGlobalPipes(new DtoValidationPipe());
     await app.init();
+
+    await userModel.deleteOne({ username: user.username });
   });
 
-  it('/ (GET)', () => {
+  it('/api/auth/register (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/api/auth/register')
+      .send({ username: user.username, email: user.email, password: '12345678' })
+      .expect(201)
+      .expect({ code: StatusCode.Ok });
   });
 });
