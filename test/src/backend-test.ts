7a6880e0f5c324ffab8479c7ee44e7eb02a58076
Imperger <mr.imperger@hotmail.com>
2022-07-14 14:58:11 +0300

    test: Cover the backend login resource

--- test/src/backend-test.ts
+++ test/src/backend-test.ts
@@ -2,22 +2,24 @@ import Crypto from 'crypto';
 
 import { Api } from "./api-interface";
 import { ApiTester } from "./api-tester";
-import { NewUser, StatusCode } from "./api/backend-api";
+import { StatusCode } from "./api/backend-api";
 import { delay } from './delay';
 import { Logger } from "./logger";
 import { Mailbox } from './mailbox';
 
+const user = { cred: { username: '', email: '', password: '' } };
+
 async function registrationFlow(api: Api, logger: Logger): Promise<boolean> {
-    const username = Crypto.randomBytes(7).toString('hex');
-    const email = `${username}@dev.wsl`;
-    const user: NewUser = { username, email, password: '1234567890' };
+    user.cred.username = Crypto.randomBytes(7).toString('hex');
+    user.cred.email = `${user.cred.username}@dev.wsl`;
+    user.cred.password = '1234567890';
 
     const tester = new ApiTester(logger, 'Backend');
-    const registerResourcePass = await tester.test(() => api.backend.register(user), {
+    const registerResourcePass = await tester.test(() => api.backend.register(user.cred), {
         done: [
             {
                 If: x => x.status === 201 && x.data?.code === StatusCode.Ok,
-                Done: x => `PASS /auth/register returns { code: 0 }`
+                Done: x => `PASSED /auth/register returns { code: 0 }`
             },
             {
                 If: () => true,
@@ -40,7 +42,7 @@ async function registrationFlow(api: Api, logger: Logger): Promise<boolean> {
 
     let confirmLetter = null;
     for (let retries = 20; confirmLetter === null && retries > 0; --retries) {
-        confirmLetter = await mailbox.findByDestination(email);
+        confirmLetter = await mailbox.findByDestination(user.cred.email);
 
         await delay(5000);
     }
@@ -61,7 +63,7 @@ async function registrationFlow(api: Api, logger: Logger): Promise<boolean> {
         done: [
             {
                 If: x => x.status === 200 && x.data?.code === StatusCode.Ok,
-                Done: () => 'PASS /auth/registration/confirm registration confirmed successfully'
+                Done: () => 'PASSED /auth/registration/confirm registration confirmed successfully'
             },
             {
                 If: () => true,
@@ -79,11 +81,60 @@ async function registrationFlow(api: Api, logger: Logger): Promise<boolean> {
     return registerResourcePass && confirmResourcePass;
 }
 
+async function signinFlow(api: Api, logger: Logger): Promise<boolean> {
+    const tester = new ApiTester(logger, 'Backend');
+
+    const loginUsernamePass = await tester.test(
+        () => api.backend.loginUsername(user.cred.username, user.cred.password),
+        {
+            done: [
+                {
+                    If: x => x.status === 200 && x.data?.code === StatusCode.Ok && x.data?.token?.length > 0,
+                    Done: () => 'PASS /auth/login/username returns user token'
+                },
+                {
+                    If: () => true,
+                    Throw: x => `FAILED /auth/login/username should return user token. Given: '${x.data}'`
+                }
+            ],
+            error: [
+                {
+                    If: () => true,
+                    Throw: x => `FAILED /auth/login/username should return 200. Given: ${x.response?.status}`
+                }
+            ]
+        });
+
+        const loginEmailPass = await tester.test(
+            () => api.backend.loginEmail(user.cred.email, user.cred.password),
+            {
+                done: [
+                    {
+                        If: x => x.status === 200 && x.data?.code === StatusCode.Ok && x.data?.token?.length > 0,
+                        Done: () => 'PASS /auth/login/email returns user token'
+                    },
+                    {
+                        If: () => true,
+                        Throw: x => `FAILED /auth/login/email should return user token. Given: '${x.data}'`
+                    }
+                ],
+                error: [
+                    {
+                        If: () => true,
+                        Throw: x => `FAILED /auth/login/email should return 200. Given: ${x.response?.status}`
+                    }
+                ]
+            });
+
+    return loginUsernamePass && loginEmailPass;
+}
+
 export async function testBackend(api: Api,): Promise<boolean> {
     const logger = new Logger('Backend');
 
     let success = true;
     success = await registrationFlow(api, logger) && success;
+    success = await signinFlow(api, logger) && success;
 
     return success;
 }
