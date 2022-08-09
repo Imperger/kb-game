05247429b354b705024d9650a1ce2891c70dbab5
Imperger <mr.imperger@hotmail.com>
2022-08-09 09:15:19 +0300

    fix: Correct status code for a unknown player

--- backend/src/player/pipes/player-by-nickname.pipe.ts
+++ backend/src/player/pipes/player-by-nickname.pipe.ts
@@ -1,4 +1,4 @@
-import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -22,7 +22,7 @@ export class PlayerByNicknamePipe implements PipeTransform {
       .collation({ locale: 'en', strength: 1 });
 
     if (player == null) {
-      throw new BadRequestException('Unknown player');
+      throw new NotFoundException('Unknown player');
     }
 
     return player;
