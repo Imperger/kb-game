d1da63d43bc1a1bd3ec2aece7f8d547538a24e9f
Imperger <mr.imperger@hotmail.com>
2022-08-19 10:25:04 +0300

    feat: Possibility to redirect a quick game players to dev instance

--- spawner/src/spawner/spawner.module.ts
+++ spawner/src/spawner/spawner.module.ts
@@ -2,12 +2,28 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { DockerService } from './docker.service';
 import { InstanceNameResolverService } from './instance-name-resolver.service';
+import { QuickToDevRedirector } from './interceptors/quick-to-dev-redirector';
 
 import { SpawnerService } from './spawner.service';
 
+function spawnerServiceInjector() {
+    switch (process.env.NODE_ENV) {
+        case 'quick-redirect':
+            return QuickToDevRedirector;
+        default:
+            return SpawnerService;
+    }
+}
+
 @Module({
     imports: [HttpModule],
-    providers: [InstanceNameResolverService, SpawnerService, DockerService],
+    providers: [
+        InstanceNameResolverService,
+        {
+            provide: SpawnerService,
+            useClass: spawnerServiceInjector()
+        },
+        DockerService],
     exports: [SpawnerService]
 })
 export class SpawnerModule {
