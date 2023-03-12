197c1b965ea28e2c7db9d9c5354f52ce64406858
Imperger <mr.imperger@hotmail.com>
2023-03-12 13:30:43 +0200

    chore: Make a debug game instance visible in the server browser

--- spawner/src/spawner/interceptors/custom-to-dev-redirector.ts
+++ spawner/src/spawner/interceptors/custom-to-dev-redirector.ts
@@ -1,10 +1,29 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import { HttpService } from "@nestjs/axios";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
+import { DockerService } from "../docker.service";
+import { InstanceNameResolverService } from "../instance-name-resolver.service";
 
-import { CustomGameOptions, InstanceRequestResult, SpawnerService } from "../spawner.service";
+import { 
+  CustomGameOptions, 
+  InstanceRequestResult, 
+  InstanceType, 
+  SpawnerService } from "../spawner.service";
 
 @Injectable()
 export class CustomToDevRedirector extends SpawnerService {
+    constructor(configService: ConfigService,
+      dockerService: DockerService,
+      http: HttpService,
+      jwtService: JwtService,
+      instanceNameResolver: InstanceNameResolverService) {
+      super(configService, dockerService, http, jwtService, instanceNameResolver);
+
+      this.instancesHost.set('ga_00000000000000000000000000000000', InstanceType.Custom);
+    }
+
     async createCustomGame(options: CustomGameOptions): Promise<InstanceRequestResult | null> {
-        return { instanceUrl: 'wss://spawner.dev.wsl:3001/random_generated_instance_id' }
+        return { instanceUrl: 'wss://spawner.dev.wsl:3001/00000000000000000000000000000000' }
     }
 }
--- spawner/src/spawner/interceptors/quick-to-dev-redirector.ts
+++ spawner/src/spawner/interceptors/quick-to-dev-redirector.ts
@@ -1,10 +1,29 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import { HttpService } from "@nestjs/axios";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
+import { DockerService } from "../docker.service";
+import { InstanceNameResolverService } from "../instance-name-resolver.service";
 
-import { InstanceRequestResult, QuickGameOptions, SpawnerService } from "../spawner.service";
+import { 
+    InstanceRequestResult, 
+    InstanceType, 
+    QuickGameOptions, 
+    SpawnerService } from "../spawner.service";
 
 @Injectable()
 export class QuickToDevRedirector extends SpawnerService {
+    constructor(configService: ConfigService,
+        dockerService: DockerService,
+        http: HttpService,
+        jwtService: JwtService,
+        instanceNameResolver: InstanceNameResolverService) {
+        super(configService, dockerService, http, jwtService, instanceNameResolver);
+  
+        this.instancesHost.set('ga_00000000000000000000000000000000', InstanceType.Custom);
+      }
+      
     async createQuickGame(options: QuickGameOptions): Promise<InstanceRequestResult | null> {
-        return { instanceUrl: 'wss://spawner.dev.wsl:3001/random_generated_instance_id' }
+        return { instanceUrl: 'wss://spawner.dev.wsl:3001/00000000000000000000000000000000' }
     }
 }
