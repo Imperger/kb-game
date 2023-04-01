2afcf385db305bfc2ce7b634c3dfe9e3d5b794a8
Imperger <mr.imperger@hotmail.com>
2023-04-01 20:49:38 +0300

    chore: Setup linter for test project

---
+++ test/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: 'tsconfig.json',
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier'
+    ],
+    root: true,
+    env: {
+      node: true,
+      jest: true,
+    },
+    rules: {
+      'indent': 'off',
+      "comma-dangle": "off",
+      "@typescript-eslint/comma-dangle": ["error"],
+      '@typescript-eslint/indent': ['error', 2],
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      "@typescript-eslint/explicit-module-boundary-types": "off"
+    },
+  };
+  
