35dd1e94c36201ecd76da93dfc12991fe77ded0a
Imperger <mr.imperger@hotmail.com>
2023-04-01 21:21:20 +0300

    chore: Tune eslint rules

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -23,6 +23,12 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    "@typescript-eslint/explicit-module-boundary-types": "off"
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { "argsIgnorePattern": "^_" }
+    ],
+    "arrow-parens" : ["warn", "as-needed", { "requireForBlockBody": false }]
   },
 };
